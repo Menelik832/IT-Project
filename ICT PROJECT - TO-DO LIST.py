9ba41a923083aa2fcdebb4fc6daf8f6dd26aa7cf
Menelik832 <menelik.tadesse00@gmail.com>
2025-12-05 10:01:35 +0300

    Update and rename ICT PROJECT - TO-DO LIST to ICT PROJECT - TO-DO LIST.py

---
+++ ICT PROJECT - TO-DO LIST.py
@@ -0,0 +1,119 @@
+to_do = []
+completed = []
+
+def add():
+    new = input("Add new task: ").strip()
+    if new:
+        to_do.append(new)
+        print(f'Added: "{new}"')
+    else:
+        print("ENTER A TASK")
+
+def remove():
+    if not to_do:
+        print("No tasks to remove")
+        return
+    show_list()
+    rem = input ("What task should we remove? ").strip()
+    if rem in to_do:
+        to_do.remove(rem)
+        print(f'Removed: "{rem}"')
+    else:
+        print(f'"{rem}" not found in your list.')
+
+def edit():
+    if not to_do:
+        print("No tasks to edit")
+        return
+    show_list()
+    original = input("Which task would you like to change? ").strip()
+    if original in to_do:
+        index = to_do.index(original)
+        changed = input("Enter the amended version: ").strip()
+        if changed:
+            to_do[index] = changed 
+            print(f'Updated: "{original}" to "{changed}"')
+        else:
+            print("Task has been repeated. Edit removed.")
+    else:
+        print(f'"{original}" not found.')
+
+def mark_done():
+    if not to_do:
+        print("No tasks available")
+        return
+    show_list()
+    done = input("Which task has been complete? ").strip()
+    if done in to_do:
+        to_do.remove(done)
+        completed.append(done)
+        print(f'Marked as completed: "{done}"')
+    else:
+        print(f'"{done}" not found.')
+
+def show_list():
+    if not to_do:
+        print("No tasks available")
+    else:
+        print("Your task list:")
+        for i, task in enumerate(to_do, 1):
+            print(f"{i}. {task}")
+        print()
+
+def show_completed():
+    if not completed: 
+        print("No completed tasks")
+    else:
+        print("Completed tasks:")
+        for i, task in enumerate(completed, 1):
+            print(f"{i}. {task}")
+        print()
+
+def clear_all():
+    to_do.clear()
+    completed.clear()
+    print("All tasks removed")
+
+def main_menu():
+    print("How Can I Help You Today?")
+    print("1. Add a new task")
+    print("2. Remove a task")
+    print("3. Edit a task")
+    print("4. Mark a task completed")
+    print("5. Show all tasks")
+    print("6. Show all completed tasks")
+    print("7. Remove all tasks")
+    print("8. Exit")
+
+def main():
+    while True:
+        main_menu()
+        choice = input("> ").strip()
+
+        if choice == '1':
+            add()
+        elif choice == '2':
+            remove()
+        elif choice == '3':
+            edit()
+        elif choice == '4':
+            mark_done()
+        elif choice == '5':
+            show_list()
+        elif choice == '6':
+            show_completed()
+        elif choice == '7':
+            clear_all()
+        elif choice == '8':
+            print("Have a Great Rest of Your Day!")
+            break
+        else:
+            print("Invaled input. Pick 1-8")
+main()
+    
+    
+
+
+
+    
+
